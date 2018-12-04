6001cf3d7cba0182c3365b5d72507c0fb589ab8b
bdelkayam <bdelkayam@gmail.com>
2018-12-04 03:50:01 +0200

    התחלתי

--- app/src/main/java/com/jct/bd/gettexidriver/model/backend/IDB_Backend.java
+++ app/src/main/java/com/jct/bd/gettexidriver/model/backend/IDB_Backend.java
@@ -1,5 +1,19 @@
 package com.jct.bd.gettexidriver.model.backend;
 
+import com.jct.bd.gettexidriver.model.entities.Driver;
+import com.jct.bd.gettexidriver.model.entities.Ride;
+
+import java.util.Date;
+import java.util.List;
+
 public interface IDB_Backend {
-    
+    public void addDriver(Driver driver);
+    public List<Ride> availableRides();
+    public List<Ride> finishedRides();
+    public List<Ride> specificDriverRides(Driver driver);
+    public List<Ride> availableRidesOnCity(String city);
+    public List<Ride> availableRidesfromDriver(Driver driver);
+    public List<Ride> dateRides(Date date);
+    public List<String> DriversUserName();
+    public List<Ride> paymentRides();
 }
