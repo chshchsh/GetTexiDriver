17edac5b64ef7c34f6c3c14ab7412ab424c02e54
bdelkayam <bdelkayam@gmail.com>
2018-12-16 00:29:27 +0200

    sd

--- app/src/main/java/com/jct/bd/gettexidriver/controller/MyService.java
+++ app/src/main/java/com/jct/bd/gettexidriver/controller/MyService.java
@@ -3,20 +3,42 @@ package com.jct.bd.gettexidriver.controller;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
+    boolean isThreadOn = false;
     public final String TAG = "myService";
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d(TAG," onCreate");
+
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return flags;
+        if(!isThreadOn)
+        {
+            isThreadOn = true;
+            SumCalc sumCalc = new SumCalc();
+            sumCalc.start();
+        }
+        return START_STICKY;
     }
     @Override
     public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG," onDestroy");
+
+    }
+    public class SumCalc extends Thread {
+
+        public void run() {
+
+            isThreadOn = false;
+
+        }
     }
     @Override
     public IBinder onBind(Intent intent) {
