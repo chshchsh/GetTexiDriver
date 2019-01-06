05414e52cdb3db6d4d1a84eb40f53c2f56880e57
bdelkayam <bdelkayam@gmail.com>
2019-01-06 16:20:29 +0200

    ds

--- app/src/main/java/com/jct/bd/gettexidriver/model/backend/MyReceiver.java
+++ app/src/main/java/com/jct/bd/gettexidriver/model/backend/MyReceiver.java
@@ -1,5 +1,6 @@
 package com.jct.bd.gettexidriver.model.backend;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -18,22 +19,31 @@ import static android.support.v4.content.ContextCompat.getColor;
 public class MyReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+        initChannels(context);
         PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
-                new Intent(context, LoginActivity.class),0);
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setContentTitle(context.getString(R.string.relevent_ride))
+                new Intent(context, LoginActivity.class), 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "default");
+                builder.setContentTitle(context.getString(R.string.relevent_ride))
                 .setContentText(context.getString(R.string.new_reqest));
         builder.setContentIntent(contentIntent);
         builder.setDefaults(NotificationCompat.DEFAULT_ALL);
         builder.setAutoCancel(true);
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             builder.setSmallIcon(R.drawable.ic_directions_car_black_24dp);
-            builder.setColor(getColor(context,R.color.bg_login));
+            builder.setColor(getColor(context, R.color.bg_login));
         } else {
             builder.setSmallIcon(R.drawable.ic_directions_car_black_24dp);
         }
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1,builder.build());
+        notificationManager.notify(1, builder.build());
+    }
+    public  void initChannels(Context context) {
+        if (Build.VERSION.SDK_INT < 26) {
+            return;
+        }
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationChannel channel = new NotificationChannel("default", "Channel name", NotificationManager.IMPORTANCE_DEFAULT);
+        notificationManager.createNotificationChannel(channel);
     }
 }
--- app/src/main/java/com/jct/bd/gettexidriver/model/backend/MyService.java
+++ app/src/main/java/com/jct/bd/gettexidriver/model/backend/MyService.java
@@ -9,36 +9,42 @@ import com.jct.bd.gettexidriver.controller.MainActivity;
 import com.jct.bd.gettexidriver.model.datasource.NotifyDataChange;
 import com.jct.bd.gettexidriver.model.entities.Ride;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(getApplicationContext()," Service Create",Toast.LENGTH_LONG);
+        Toast.makeText(getApplicationContext(), " Service Create", Toast.LENGTH_LONG);
 
     }
+
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-                FactoryBackend.getInstance().notifyToRideList(new NotifyDataChange<List<Ride>>() {
-                    @Override
-                    public void OnDataChanged(List<Ride> obj) {
-                        Intent intent = new Intent(MyService.this,MyReceiver.class);
-                        sendBroadcast(intent);
-                    }
+        FactoryBackend.getInstance().notifyToRideList(new NotifyDataChange<List<Ride>>() {
+            @Override
+            public void OnDataChanged(List<Ride> obj) {
+                Intent intent = new Intent(MyService.this, MyReceiver.class);
+                sendBroadcast(intent);
+            }
 
-                    @Override
-                    public void onFailure(Exception exception) {
-                    }
-                });
+            @Override
+            public void onFailure(Exception exception) {
+            }
+        });
         return START_STICKY;
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
         FactoryBackend.getInstance().stopNotifyToRidesList();
-        Toast.makeText(getApplicationContext()," Service destroy",Toast.LENGTH_LONG);
+        Toast.makeText(getApplicationContext(), " Service destroy", Toast.LENGTH_LONG);
     }
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
